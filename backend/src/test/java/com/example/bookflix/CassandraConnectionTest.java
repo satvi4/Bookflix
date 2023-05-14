c19d5e56f89c6667b622a4fd5232fef8e8867224
Satvi4 <satvika144@gmail.com>
2023-05-14 19:21:43 +0530

    Cassandra connection test

---
+++ backend/src/test/java/com/example/bookflix/CassandraConnectionTest.java
@@ -0,0 +1,27 @@
+package com.example.bookflix;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.nio.file.Path;
+
+@SpringBootTest
+class CassandraConnectionTest {
+
+    @Value("${cassandra.secure-connect-bundle}")
+    private Path secureConnectBundlePath;
+
+    @Test
+    void testCassandraConnection() {
+        try (CqlSession session = CqlSession.builder()
+                .withCloudSecureConnectBundle(secureConnectBundlePath)
+                .build()) {
+            assertNotNull(session);
+            
+        }
+    }
+}
